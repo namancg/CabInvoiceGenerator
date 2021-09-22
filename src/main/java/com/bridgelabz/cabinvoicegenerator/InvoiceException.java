bb59c424f6446859fd063f5fde0ad8e238ab9a46
namancg <naman.c@perfios.com>
2021-09-22 15:10:03 +0530

    ADDED: custom exceptions

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceException.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.cabinvoicegenerator;
+public class InvoiceException extends Exception {
+    public enum ExceptionType {
+     USER_NOT_EXIST, USER_ALREADY_EXISTS
+    }
+
+    public ExceptionType type;
+
+    public InvoiceException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+}
