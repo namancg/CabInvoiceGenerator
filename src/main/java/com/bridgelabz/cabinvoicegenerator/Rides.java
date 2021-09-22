70fbcebaf663ddd43779490b7a41f9efa34391df
namancg <naman.c@perfios.com>
2021-09-22 15:07:11 +0530

    REFACTORED: set and get rideType

--- src/main/java/com/bridgelabz/cabinvoicegenerator/Rides.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Rides.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import com.bridgelabz.cabinvoicegenerator.InvoiceGenerator.RideType;
+
 public class Rides {
 	 private int time;
 	 private double distance;
-	  public Rides(double distance, int time) {
+	 private InvoiceGenerator.RideType rideType;
+	  public Rides(double distance, int time, RideType rideType) {
 	        this.distance = distance;
 	        this.time = time;
+	        this.rideType = rideType;
 	    }
 	  public int getTime() {
 	        return time;
@@ -21,5 +25,12 @@ public class Rides {
 
 	    public void setDistance(double distance) {
 	        this.distance = distance;
+	    }
+	    public InvoiceGenerator.RideType getRideType() {
+	        return rideType;
+	    }
+
+	    public void setRideType(InvoiceGenerator.RideType rideType) {
+	        this.rideType = rideType;
 	    }
 }
