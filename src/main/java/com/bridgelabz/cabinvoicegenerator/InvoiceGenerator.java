997ad489ee174141fa5e91894871bbe63121dd44
namancg <naman.c@perfios.com>
2021-09-22 13:42:59 +0530

    REFACTORED: method to add rides and return invoice

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -4,7 +4,10 @@ public class InvoiceGenerator {
 	  private static final double MINIMUM_COST_PER_KILOMETER = 10;
 	    private static final int COST_PER_TIME = 1;
 	    private static final int MIN_FARE = 5;
-	    
+	    private RideRepository rideRepository;
+	    public InvoiceGenerator() {
+	        this.rideRepository = new RideRepository();
+	    }
 	    public double calculateFare(double distance, int time) {
 	        double totalFare = distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
 	        if (totalFare< MIN_FARE)
@@ -25,6 +28,12 @@ public class InvoiceGenerator {
 	           
 	        }
 	        return new InvoiceSummary(rides.length, totalFare);
+	    }
+	    public void addRides(String userId, Rides[] rides) {
+	        rideRepository.addRides(userId, rides);
+	    }
+	    public InvoiceSummary getInvoiceSummary(String userId) {
+	        return this.calculateTotalFare(rideRepository.getRides(userId));
 	    }
 	    
 
