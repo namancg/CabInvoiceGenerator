f40c0a5d95dc4fa2b797b6163dde64fef3cd205d
namancg <naman.c@perfios.com>
2021-09-22 12:42:27 +0530

    ADDED: test cases to generate invoice for multiple rides

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -4,10 +4,11 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class InvoiceServiceTest {
+	 InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 	  @Test
 	    public void givenDistanceAndTime_ShouldReturnTotalFare()
 	  {
-	        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+	       // InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 	        double distance = 2.0;
 	        int time = 5;
 	        double fare = invoiceGenerator.calculateFare(distance, time);
@@ -16,10 +17,18 @@ public class InvoiceServiceTest {
 	  @Test
 	    public void givenLessDistanceAndTime_ShouldReturnMinFare() 
 	  {
-	        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+	       // InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 	        double distance = 0.1;
 	        int time = 1;
 	        double fare = invoiceGenerator.calculateFare(distance, time);
 	        Assert.assertEquals(5, fare,0.0);
 	   }
+	  @Test
+	    public void givenMultipleRides_shouldReturnTotalFare() {
+	        Rides[] rides = { new Rides(2.0, 5),
+	                         new Rides(0.1, 1)
+	                        };
+	        double fare = invoiceGenerator.calculateFare(rides);
+	        Assert.assertEquals(30, fare, 0.0);
+	    }
 }
